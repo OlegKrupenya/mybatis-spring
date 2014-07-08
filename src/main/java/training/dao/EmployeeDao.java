41c0bae84f57f6f9a0086d9e9a68f0155b066246
Oleg Krupenya <oleg.krupenya@akvelon.com>
2014-07-09 01:09:46 +0300

    cache

--- src/main/java/training/dao/EmployeeDao.java
+++ src/main/java/training/dao/EmployeeDao.java
@@ -24,7 +24,7 @@ public class EmployeeDao implements IEmployeeDao {
     public List<Employee> getEmployees() {
         List<Employee> employees = null;
         employees = sqlSession
-                .selectList("com.testdev.dao.EmployeeMapper.selectEmployeesWithCarsAndCompanies");
+                .selectList("training.dao.EmployeeMapper.selectEmployeesWithCarsAndCompanies");
         return employees;
     }
 
@@ -34,24 +34,22 @@ public class EmployeeDao implements IEmployeeDao {
         Map<String, Object> params = new HashMap<>();
         params.put("id", employeeId);
         employee = sqlSession.selectOne(
-                "com.testdev.dao.EmployeeMapper.selectEmployee", params);
+                "training.dao.EmployeeMapper.selectEmployee", params);
         return employee;
     }
 
     @Override
     public Employee addEmployee(Employee employeeToAdd) {
-        sqlSession.insert("com.testdev.dao.EmployeeMapper.insertEmployee",
+        sqlSession.insert("training.dao.EmployeeMapper.insertEmployee",
                 employeeToAdd);
-        sqlSession.commit();
         return employeeToAdd;
     }
 
     @Override
     public boolean updateEmployee(Employee employeeToUpdate) {
         try {
-            sqlSession.update("com.testdev.dao.EmployeeMapper.updateEmployee",
+            sqlSession.update("training.dao.EmployeeMapper.updateEmployee",
                     employeeToUpdate);
-            sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -62,9 +60,8 @@ public class EmployeeDao implements IEmployeeDao {
     @Override
     public boolean deleteEmployee(Employee employeeToDelete) {
         try {
-            sqlSession.update("com.testdev.dao.EmployeeMapper.deleteEmployee",
+            sqlSession.update("training.dao.EmployeeMapper.deleteEmployee",
                     employeeToDelete);
-            sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
             return false;
